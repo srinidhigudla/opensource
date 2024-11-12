d60338e4c7422bcf1c45277e45f0ff477cc4bbe8
srinidhigudla <srinidhigudla21@gmail.com>
2024-11-12 10:45:42 +0530

    Create Main.java

---
+++ eb_dsa/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class solution{
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    if(N%7==0) {
+      System.out.println("lucky");
+    }
+    else{
+      System.out.println("unlucky");
+    }
+  }
+}
